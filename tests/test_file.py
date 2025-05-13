1e56a38a3595681057c24ab2df88017a1204315b
bradley-dev777 <hu.yu@wcs-g.com>
2025-05-13 21:53:12 +0000

    yeah... this again

--- tests/test_file.py
+++ tests/test_file.py
@@ -3,7 +3,8 @@ spec = importlib.util.spec_from_file_location("HTMLBuilder", "./src/Html_Builder
 module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
 HTMLBuilder = module.HTMLBuilder
-obj = HTMLBuilder()
-obj.doctype(obj)
-obj.html(obj,obj.head("")+obj.body(obj,obj.h1(obj,"Title test")+obj.p(obj,"Paragraph")))
-print(obj.get_html())
+def test_1():
+    obj = HTMLBuilder()
+    obj.doctype(obj)
+    obj.html(obj,obj.head("")+obj.body(obj,obj.h1(obj,"Title test")+obj.p(obj,"Paragraph")))
+    print(obj.get_html())
